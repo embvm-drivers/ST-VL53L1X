b38669cd0dade4eb539a5e1923c33fc77784e89d
Phillip Johnston <phillip@embeddedartistry.com>
2020-11-03 15:44:50 -0800

    Add copyright and license header to source files.

--- src/vl53l1x.cpp
+++ src/vl53l1x.cpp
@@ -1,3 +1,6 @@
+// Copyright 2020 Embedded Artistry LLC
+// SPDX-License-Identifier: MIT
+
 #include "vl53l1x.hpp"
 #include "vl53l1x_registers.h"
 #include <cassert>
--- src/vl53l1x.hpp
+++ src/vl53l1x.hpp
@@ -1,3 +1,6 @@
+// Copyright 2020 Embedded Artistry LLC
+// SPDX-License-Identifier: MIT
+
 #ifndef ST_V53L1X_HPP_
 #define ST_V53L1X_HPP_
 
