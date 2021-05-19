f5226e0ec4b286143c86b4326847ae16e0a0574e
Phillip Johnston <phillip@embeddedartistry.com>
2021-05-19 13:42:02 -0700

    Add a test case for our recorded data format
    This test is used to ensure I can copy + paste a line from the logged data and generate a proper I2C operation. It does not functionally test this driver.

--- test/catch2_test_case.cpp
+++ test/catch2_test_case.cpp
@@ -1,8 +1,22 @@
 #include <catch2/catch.hpp>
+#include <string.h>
 #include <vl53l1x.hpp>
 
-TEST_CASE("Sanity Check", "[test/catch2_test_case]")
+using namespace embvm::i2c;
+
+TEST_CASE("Create an I2C Operation - Check Recorded Data Format", "[st-vl53l1x/recording]")
 {
-	CHECK(1 == 1);
-	STATIC_REQUIRE(1 == 1);
+	uint8_t ref[2] = {0x01, 0x0f};
+	embvm::i2c::op_t op = {.address = 0x29,
+						   .op = operation::writeRead,
+						   .tx_size = 2,
+						   .tx_buffer = new uint8_t[]{0x1, 0xf},
+						   .rx_size = 1,
+						   .rx_buffer = new uint8_t[]{0xea}};
+	CHECK(op.address == 0x29);
+	CHECK(op.op == embvm::i2c::operation::writeRead);
+	CHECK(op.tx_size == 2);
+	CHECK(0 == memcmp(op.tx_buffer, ref, op.tx_size));
+	delete[] op.tx_buffer;
+	delete[] op.rx_buffer;
 }
